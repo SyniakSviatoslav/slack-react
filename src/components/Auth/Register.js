f97222c77d35389399edfe0bcc795416e7d8d9bb
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-12-03 20:16:51 +0200

    added authentication for register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,59 @@
 import React from "react";
 import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import firebase from '../../firebase';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 class Register extends React.Component {
-    state = {}
-    handleChange = () => { }
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+
+    // handleSubmit = event => {
+    //     event.preventDefault();
+    //     firebase
+    //         .auth()
+    //         .createUserWithEmailAndPassword(this.state.email, this.state.password)
+    //         .then(createdUser => {
+    //             console.log(createdUser);
+    //         })
+    //         .catch(err => {
+    //             console.error(err)
+    //         })
+    // }
+
+    handleSubmit = event => {
+        event.preventDefault();
+      
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, this.state.email, this.state.password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+      
+    }
+
+  
+
+
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    };
+
     render() {
+        const { username, email, password, passwordConfirmation } = this.state;
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
@@ -13,16 +61,19 @@ class Register extends React.Component {
                         <Icon name="puzzle piece" color="orange" />
                         Register for DevChat
                     </Header>
-                    <Form size="large">
+                    <Form onSubmit={this.handleSubmit} size="large">
                         <Segment stacked>
                             <Form.Input fluid name="username" icon="user" iconPosition="left"
-                                placeholder="Username" onChange={this.handleChange} type="text" />
+                                placeholder="Username" onChange={this.handleChange} type="text" value={username} />
 
                             <Form.Input fluid name="email" icon="mail" iconPosition="left"
-                                placeholder="Email Address" onChange={this.handleChange} type="email" />
+                                placeholder="Email Address" onChange={this.handleChange} type="email" value={email} />
+
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left"
+                                placeholder="Password" onChange={this.handleChange} type="password" value={password} />
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
-                                placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+                                placeholder="Password Confirmation" onChange={this.handleChange} type="password" value={passwordConfirmation} />
 
                             <Button color="orange" fluid size="large">Sumbit</Button>
                         </Segment>
--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,41 @@
-import firebase from 'firebase/app';
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
+// import firebase from 'firebase/compat/app';
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyDQR1eTapY2jDJX_OVO0AgRYQPgI9YgJhM',
+  authDomain: 'react-slack-8ed42.firebaseapp.com',
+  projectId: 'react-slack-8ed42"'
+});
+
+const db = getFirestore();
 
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+// //   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-analytics.js";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQR1eTapY2jDJX_OVO0AgRYQPgI9YgJhM",
-    authDomain: "react-slack-8ed42.firebaseapp.com",
-    projectId: "react-slack-8ed42",
-    storageBucket: "react-slack-8ed42.appspot.com",
-    messagingSenderId: "663292970597",
-    appId: "1:663292970597:web:13426eb99842d874d6688c",
-    measurementId: "G-X6K6J8HNHE"
-  };
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyDQR1eTapY2jDJX_OVO0AgRYQPgI9YgJhM",
+//     authDomain: "react-slack-8ed42.firebaseapp.com",
+//     projectId: "react-slack-8ed42",
+//     storageBucket: "react-slack-8ed42.appspot.com",
+//     messagingSenderId: "663292970597",
+//     appId: "1:663292970597:web:13426eb99842d874d6688c",
+//     measurementId: "G-X6K6J8HNHE"
+//   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
 
-  export default firebase;
+  export default firebaseApp;
+//   export default firebaseApp;
