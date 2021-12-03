1ee5e1b1680dd59d84f4ff20c91aec8d57f99146
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2021-12-03 19:18:23 +0200

    added styles for register

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends React.Component {
+    state = {}
+    handleChange = () => { }
     render() {
         return (
-            <div>Register</div>
-        )
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as="h2" icon color="orange" textAlign="center">
+                        <Icon name="puzzle piece" color="orange" />
+                        Register for DevChat
+                    </Header>
+                    <Form size="large">
+                        <Segment stacked>
+                            <Form.Input fluid name="username" icon="user" iconPosition="left"
+                                placeholder="Username" onChange={this.handleChange} type="text" />
+
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left"
+                                placeholder="Email Address" onChange={this.handleChange} type="email" />
+
+                            <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
+                                placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+
+                            <Button color="orange" fluid size="large">Sumbit</Button>
+                        </Segment>
+                    </Form>
+                    <Message>Already a user?<Link to="/login"> Login</Link></Message>
+                </Grid.Column>
+            </Grid>
+
+        );
     }
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQR1eTapY2jDJX_OVO0AgRYQPgI9YgJhM",
+    authDomain: "react-slack-8ed42.firebaseapp.com",
+    projectId: "react-slack-8ed42",
+    storageBucket: "react-slack-8ed42.appspot.com",
+    messagingSenderId: "663292970597",
+    appId: "1:663292970597:web:13426eb99842d874d6688c",
+    measurementId: "G-X6K6J8HNHE"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  export default firebase;
